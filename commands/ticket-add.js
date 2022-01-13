0c47a16c685e618f6e1c429739e484f6643d90c4
Glitch (hello-express) <none>
2022-01-13 19:27:29 +0000

    🦂⛺️ Checkpoint ./commands/ticket-add.js:22224612/1345 ./commands/ticket-close.js:22224612/6904

--- commands/ticket-add.js
+++ commands/ticket-add.js
@@ -1,30 +1,35 @@
-const Discord = require('discord.js');
-const data = require('quick.db')
+const db = require("quick.db")
+const Discord = require('discord.js')
 
-exports.run = async (client, message, args) => {
-const prefix = await data.fetch(`prefix.${message.guild.id}`) || client.ayarlar.prefix;
+module.exports = {
+    
+    isim: "ticket-add", 
+    alternatifler: [],
+     kullanımı:"help",
+
+    çalıştır: async (client, message, args, interaction) => {
   
 if(!args[0]) {
-message.delete()
-return message.channel.send(`Komutu düzgün kullanın: \`[Etiket/Rol] (kanal)\``).then(c => c.delete(10000)) }
+
+return message.channel.send(`Komutu düzgün kullanın: \`?ticket-add [Etiket/Rol] (kanal)\``) }
   
 if(args[0]) {
 
 if(!args[1]) {
-const ad = await data.fetch(`numara.${message.channel.id}`)
+const ad = await db.fetch(`numara.${message.channel.id}`)
 if(!message.channel.name === `ticket-${ad}` || message.channel.name === `closed-${ad}`) {
-const ann = await data.fetch(`asd.${message.guild.id}.${message.channel.id}.${message.author.id}`)
+const ann = await db.fetch(`asd.${message.guild.id}.${message.channel.id}.${message.author.id}`)
 if(!ann) return message.channel.send(`Bu bilet senin değil.`)
-message.delete()
+
 return message.channel.send(`Bu komutu bir bilet kanalında kullanın.`).then(c => c.delete(5000)) } }
   
 let mt = message.mentions.roles.first() || message.mentions.users.first()
 if(!mt) {
-message.delete()
+
 return message.channel.send(`Komutu düzgün kullanın: \`[Etiket/Rol] (kanal)\``)  }
 let mention = message.guild.roles.cache.get(mt.id) || message.guild.members.cache.get(mt.id)
 if(!mention) {
-message.delete()
+
 return message.channel.send(`Komutu düzgün kullanın: \`[Etiket/Rol] (kanal)\``)  }  
   
 if(args[1]) {  
@@ -32,36 +37,27 @@ if(args[1]) {
 if(message.mentions.channels.first()) {
   
 let ch = message.mentions.channels.first()
-const ad = await data.fetch(`numara.${message.channel.id}`)
+const ad = await db.fetch(`numara.${message.channel.id}`)
 if(!ch.name === `ticket-${ad}` || ch.name === `closed-${ad}`) {
-message.delete()
+
 return message.channel.send(`Bir bilet kanalı değil.`).then(c => c.delete(5000)) }  
-const ann = await data.fetch(`asd.${message.guild.id}.${ch.id}.${message.author.id}`)
+const ann = await db.fetch(`asd.${message.guild.id}.${ch.id}.${message.author.id}`)
 if(!ann) return message.channel.send(`Bu bilet senin değil.`)
-ch.createOverwrite(mt, { 'SEND_MESSAGES': true, 'VIEW_CHANNEL': true })
-message.delete()
+ch.permissionOverwrites.create(mt, { 'SEND_MESSAGES': true, 'VIEW_CHANNEL': true })
+
 const emb = new Discord.MessageEmbed()
 .setColor('GREEN')
 .setDescription(`${mt}: ${ch} kanalına eklendi.`)
-return message.channel.send(emb) } }
+return message.channel.send({embeds: [emb]}) } }
   
-message.channel.createOverwrite(mt, { 'SEND_MESSAGES': true, 'VIEW_CHANNEL': true })
-message.delete()
+message.channel.permissionOverwrites.create(mt, { 'SEND_MESSAGES': true, 'VIEW_CHANNEL': true })
+
 const emb = new Discord.MessageEmbed()
 .setColor('GREEN')
 .setDescription(`${mt}: ${message.channel} kanalına eklendi.`)
-return message.channel.send(emb)
+return message.channel.send({embeds: [emb]})
 
 }
 
-};
-exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  permLevel: 0
 }
-
-exports.help = {
-  name: 'ekle'
-};
+}
