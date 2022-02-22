a7336c7c006bb08976e6ae056da52b6f21aeea0d
Glitch (hello-express) <none>
2022-02-22 19:43:47 +0000

    ⛈🍉 Checkpoint ./package.json:22224612/1226 ./index.js:22224612/9160

---
+++ index.js
@@ -0,0 +1,104 @@
+//-------------- TANIMLAMALAR --------------
+const Discord = require('discord.js')
+
+const { Client, Util } = require('discord.js');
+const { Intents } = require('discord.js');
+const ayarlar = require('./ayarlar.json');
+const INTENTS = Object.entries(Discord.Intents.FLAGS).filter(([K]) => !["GUILD_PRESENCES", "GUILD_MEMBERS"].includes(K)).reduce((t, [, V]) => t | V, 0)
+const client = new Discord.Client({intents: INTENTS})
+const db = require("quick.db")
+require("./util/slashCommandsLoader.js")(client) 
+const { DiscordTogether } = require('discord-together');
+client.discordTogether = new DiscordTogether(client);
+
+const ayarla = require('./ayarlar.json');
+
+const fs = require("fs")
+const moment = require("moment");
+require("moment-duration-format");
+require('dotenv').config()
+
+
+//vCodes
+const vCodes = require('vcodes.js');
+//const dbl = new vCodes("jsHSKFcoGztSOMoEMlDVHpKtgANmBMaBRegRRkJwjRoajwKvehZdBmQEccTyQtORoCezlgeyHAHafslJzjqtpDZYwDbhUAXTbEFDcsOmpsRUNzYNUrPOLEOQrPQtPXOL")
+
+/*client.on("ready", () => {
+    let guildCount = client.guilds.cache.size;
+    dbl.stats(guildCount, () => console.log("[🔎 - vCodes] » vCodes.xyz'de istatistik güncellendi."));
+})
+    dbl.on("vote", ({ user, bot}) => {
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`Gölge#8629'a oy verdiğiniz için teşekkür ederiz 🎉`)
+        .setDescription(`**Kullanıcı: \`${user.username} (${user.id})\` 
+
+        Her 12 saatte bir [vCodes](https://vcodes.xyz/tr/bot/897562864859119647/vote)'ten oy verebilirsiniz.**`)
+        .setFooter({ text: `Oy verip bizi desteklediğiniz için teşükkür ederiz.`})
+        client.channels.cache.get("916997242668339240").send({embeds: [embed]})
+    })
+
+*/
+
+
+/*client.on('guildDelete', guild => {
+
+    let narcoscode = new Discord.MessageEmbed()
+    
+    .setColor("RED")
+    .setDescription(`**Bir sunucudan atıldım.
+
+    » Bilgiler;
+
+   » Sunucu Adı: \`${guild.name}\`
+   » Sunucu Sahibi: <@${guild.ownerId}>
+   » Sunucu Sahibi ID: \`${guild.ownerId}\`
+   » Sunucu ID: \`${guild.id}\`
+   » Sunucudaki Üye Sayısı: \`${guild.memberCount}\`**`)
+   .setThumbnail(guild.iconURL({dynamic: true}))
+       client.channels.cache.get(ayarlar.girdiçıktılog).send({embeds: [narcoscode]});
+      
+    });
+    
+    
+    client.on('guildCreate', guild => {
+    
+    let narcoscode = new Discord.MessageEmbed()
+
+    .setDescription(`**Bir sunucuya eklendim.
+    
+    » Bilgiler;
+
+   » Sunucu Adı: \`${guild.name}\`
+   » Sunucu Sahibi: <@${guild.ownerId}>
+   » Sunucu Sahibi ID: \`${guild.ownerId}\`
+   » Sunucu ID: \`${guild.id}\`
+   » Sunucudaki Üye Sayısı: \`${guild.memberCount}\`**`)
+   .setThumbnail(guild.iconURL({dynamic: true}))
+    
+       client.channels.cache.get(ayarlar.girdiçıktılog).send({embeds: [narcoscode]});
+      
+    });
+*/
+client.on('guildCreate', guild => {
+    process.exit(0)
+})
+
+
+
+
+
+
+
+
+
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+
+
+
+
+
+client.login(process.env.token)
+
